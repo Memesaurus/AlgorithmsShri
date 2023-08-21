4f753f04c11ee7262065844937c2bb4f3b64a5bf
gorlov <gorlov@borlas.ru>
2023-08-21 19:55:08 +0300

    validprnthsis

--- problems/valid-parenthesis/valid-parenthesis.js
+++ problems/valid-parenthesis/valid-parenthesis.js
@@ -1,5 +1,26 @@
 // https://leetcode.com/problems/valid-parentheses/
 
 export function isValid(str) {
+    const stack = [];
+    const opening = ['[', '(', '{'];
+    const closing = [']', ')', '}'];
+    const match = {
+        ']': '[',
+        ')': '(',
+        '}': '{',
+    }
 
+    for (const char of str) {
+        if (opening.includes(char)) {
+            stack.push(char);
+        } else if (closing.includes(char)) {
+            const lastEl = stack.at(-1);
+            if (lastEl !== match[char]) {
+                return false;
+            }
+            stack.pop();
+        }
+    }
+
+    return !stack.length;
 }
