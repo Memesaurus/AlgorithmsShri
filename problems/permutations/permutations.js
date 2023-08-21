5428daf933a80c03183407d38f1223f7f1b6bb2f
gorlov <gorlov@borlas.ru>
2023-08-21 21:24:06 +0300

    permutations

--- problems/permutations/permutations.js
+++ problems/permutations/permutations.js
@@ -3,5 +3,24 @@
 export function permutations(n) {
     const result = [];
 
+    const swap = (arr, a, b) => [arr[a], arr[b]] = [arr[b], arr[a]];
+
+    const permutate = (arr, idx) => {
+        for (let i = idx; i < arr.length; i++) {
+            const newArr = [...arr];
+            swap(newArr, i, idx);
+            result.push(newArr);
+            permutate(newArr, idx + 1);
+        }
+    }
+
+    const array = [];
+
+    for (let i = 1; i <= n; i++) {
+        array.push(i);
+    }
+    result.push(array);
+    
+    permutate(array, 0);
     return result;
 }
