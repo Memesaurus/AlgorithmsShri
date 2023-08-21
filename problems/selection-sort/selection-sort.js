7a4ec8e7af4de798c4b91108af3ac96668a474df
gorlov <gorlov@borlas.ru>
2023-08-21 18:45:20 +0300

    selectionSort

--- problems/selection-sort/selection-sort.js
+++ problems/selection-sort/selection-sort.js
@@ -2,6 +2,18 @@
 // https://en.wikipedia.org/wiki/Selection_sort
 
 export function selectionSort(arr) {
+    const swap = (a, b) => [arr[a], arr[b]] = [arr[b], arr[a]];
+
+    for (let i = 0; i < arr.length; i++) {
+        let minIdx = i;
+        for (let j = i; j < arr.length; j++) {
+            if (arr[j] < arr[minIdx]) {
+                minIdx = j;
+            }
+
+            swap(i, minIdx);
+        }
+    }
 
     return arr;
 }
