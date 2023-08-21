30c88b60e9b2a8df29bc67c796bb334e4c9a350b
gorlov <gorlov@borlas.ru>
2023-08-21 20:33:50 +0300

    binary

--- problems/binary-search/binary-search.js
+++ problems/binary-search/binary-search.js
@@ -1,5 +1,16 @@
 // https://en.wikipedia.org/wiki/Binary_search_algorithm
 
 export function binarySearch(arr, value) {
-    
+    let [l, r] = [0, arr.length - 1];
+
+    while (l <= r) {
+        const mid = Math.floor((l + r) / 2);
+        if (arr[mid] === value) {
+            return mid;
+        } else if (arr[mid] > value) {
+            r = mid - 1;
+        } else {
+            l = mid + 1;
+        }
+    }
 }
