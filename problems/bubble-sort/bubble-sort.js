146f57cc58f96298d918390ff72f2c9c9a02528e
gorlov <gorlov@borlas.ru>
2023-08-21 18:26:41 +0300

    bubblesort

--- problems/bubble-sort/bubble-sort.js
+++ problems/bubble-sort/bubble-sort.js
@@ -2,6 +2,17 @@
 // https://en.wikipedia.org/wiki/Bubble_sort
 
 export function bubbleSort(arr) {
+    const swap = (a, b) => [arr[a], arr[b]] = [arr[b], arr[a]];
+
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length; j++) {
+            if (arr[j] > arr[i]) {
+                swap(i, j);
+            }
+        }
+    }
 
     return arr;
 }
+
+console.log(bubbleSort([3, 2, 1]));
