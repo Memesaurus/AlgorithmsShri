eed6036a6360ef727e12c9f48b968d50dceea3d7
gorlov <gorlov@borlas.ru>
2023-08-21 20:16:33 +0300

    primenums

--- problems/prime-numbers/prime-numbers.js
+++ problems/prime-numbers/prime-numbers.js
@@ -2,6 +2,20 @@
 
 export function primeNumbers(num) {
     const result = [];
+    const nonPrimes = new Set();
+
+    for (let i = 2; i <= num; i++) {
+        let counter = i;
+        while (i * 2 <= num && counter <= num) {
+            counter += i;
+            nonPrimes.add(counter);
+        }
+
+        if (nonPrimes.has(i)) {
+            continue;
+        }
+        result.push(i);
+    }
 
     return result;
 }
