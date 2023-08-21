4666309d77aa5fe9144c14e11f434567fc764c2e
gorlov <gorlov@borlas.ru>
2023-08-21 21:31:15 +0300

    max subarray

--- problems/maximum-subarray/maximum-subarray.js
+++ problems/maximum-subarray/maximum-subarray.js
@@ -1,5 +1,16 @@
 // https://leetcode.com/problems/maximum-subarray/
 
 export function maximumSubarray(arr) {
+    let max = Math.max(...arr);
+    let currentMax = 0;
+    for (let i = 0; i < arr.length; i++) {
+        currentMax += arr[i];
+        max = Math.max(currentMax, max);
 
+        if (currentMax < 0) {
+            currentMax = 0;
+        }
+    }
+
+    return max;
 }
